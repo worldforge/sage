b61ef123e08381fa8efb92a7c13a94f709be16bf
Simon Goodall <simon@worldforge.org>
2004-04-22 19:13:03 +0000

    22nd April 2004, Simon Goodall <sage@simongoodall.co.uk>  * Changed build script to add all pointers to lib regardless of gl header  * Build sage with glext.h version 22

--- sage/utility.h
+++ sage/utility.h
@@ -1,6 +1,6 @@
 // This file may be redistributed and modified only under the terms of
 // the GNU Lesser General Public License (See COPYING for details).
-// Copyright (C) 2003 Simon Goodall
+// Copyright (C) 2003-2004 Simon Goodall
 
 #ifndef UTILITY_H
 #define UTILITY_H 1
