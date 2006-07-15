f5bc596d831051f38c88a5acfd6a380297b354f6
Simon Goodall <simon@worldforge.org>
2006-07-15 14:19:52 +0000

    2006-07-15  Simon Goodall  <sage@simongoodall.co.uk>
    	* build_sage.pl, sage/GL.h, sage/sage.h: Some changes to use SDL's
    	  opengl header instead of the system defined one. This means that we
    	  always use function pointers for gl functions to try and preservce
    	  binary compatibility between opengl versions.

--- sage/GL.h
+++ sage/GL.h
@@ -1,8 +1,10 @@
 #ifndef SAGE_GL_H
 #  define SAGE_GL_H 1
-#  ifdef __APPLE__
-#    include <OpenGL/gl.h>
-#  else
-#    include <GL/gl.h>
-#  endif
+
+#include <SDL/SDL_opengl.h>
+//#  ifdef __APPLE__
+//#    include <OpenGL/gl.h>
+//#  else
+//#    include <GL/gl.h>
+//#  endif
 #endif
